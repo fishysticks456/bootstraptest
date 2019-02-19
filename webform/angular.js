d4878b4be8487db4f6283add8a21120f9109dd88
fishysticks456 <fishysticks456@gmail.com>
2019-02-18 16:35:38 -0800

    Added Email ABT form
    - Added Email ABT form in a new tab. Includes Category Wide format and checkboxes for Discount and Free Shipping.
    - Added "Copy to Clipboard" buttons
    - Added alertUser() modal to popup at the top
    - Added localStorage functionality. Form fields and settings are saved in local Storage when the user downloads their banners. 
    
    TO DO 
    - Flyout banners

--- webform/angular.js
+++ webform/angular.js
@@ -4,18 +4,114 @@ angular.module('dymApp', [])
 .filter('resize', function() {
     return function(banner,size) {
       //console.log(size);
-      return banner.replace(/\d\d?\d?x\d\d?\d?/,size)
+      return banner.replace(/\d\d?\d?x\d\d?\d?/,size);
     };
   })
+.filter('dollars', function() {
+    return function(price) {
+      return Math.floor(price);
+    };
+  })
+.filter('cents', function() {
+    return function(price) {
+      var cents = Math.round(price % 1.0 * 100);
+      return cents < 10 ? "0" + cents : cents;
+    };
+  })
+.filter('twodecimals', function() {
+    return function(price) {
+      return price.toFixed(2);
+    };
+  })
+.filter('rounded', function() {
+    return function(price) {
+      return Math.round(price);
+    };
+  })
+.filter('scrubbed', function() {
+    return function(title) {
+      return title.replace(/"/,"&#x22;");
+    };
+  })
+.filter('stringify', function() {
+  return function(obj) {
+    return JSON.stringify(obj);
+  };
+})
+.filter('alphabetized', function() {
+  return function(text) {
+    var n = parseInt(text);
+    return  (n < 27 ?
+            String.fromCharCode(n + 64) :
+            String.fromCharCode(Math.floor((n-26) / 27) + 65) + String.fromCharCode(((n-1) % 26) + 65));
+  };
+})
+
 .controller('DYMController', function() {
   var dym = this;
+  var now = new Date();
 
   // Set default values
-  dym.campaignID = "DYM-MKS-19000";
-  dym.href = "";
-  dym.bannerLink = "https://dummyimage.com/970x50/f00/fff";
-  dym.alt = "Alt text for banners";
-  dym.customerSeg = "STATIC";
+  dym.banner = {
+    campaignID : "DYM-MKS-19000",
+    href : "",
+    bannerLink : "https://dummyimage.com/970x50/f00/fff",
+    alt : "Alt text for banners",
+    customerSeg : "STATIC",
+    settings : {
+      precode : false,
+      preview : true
+    }
+  };
+
+  dym.email = {
+    campaignID : "DYMX-ABT-17011",
+    date : now.getDate() + (now.getMonth() < 9 ? "0" + (now.getMonth()+1) : now.getMonth()+1) + now.getFullYear(),
+    sku : "20-147-674",
+    producttitle : "SAMSUNG 860 EVO Series 2.5\" 500GB SATA III V-NAND 3-Bit MLC Internal SSD",
+    productimg : "https://c1.neweggimages.com/NeweggImage/ProductImageCompressAll300/20-147-674-V01.jpg",
+    promotext : "Extra savings with promocode",
+    fullprice : 100.00,
+    saleprice : 80.00,
+    isCategory : false,
+    freeShipping : true,
+    showDiscount : true,
+    count : 260
+    
+  };
+  
+  dym.copy = {
+    banner : {},
+    email : {}
+  };
+  
+  
+  
+  // Store data in localStorage
+  dym.save = function() {
+    for( var m in dym.banner ) {dym.copy.banner[m] = dym.banner[m];}
+    for( m in dym.email ) {dym.copy.email[m] = dym.email[m];}
+    localStorage.setItem("dym", JSON.stringify(dym.copy));
+    
+    alertUser("Saved dym session data to local storage");
+  };
+  dym.load = function() {
+    dym.copy = JSON.parse(localStorage.getItem("dym"));
+    for( var m in dym.banner ) {dym.banner[m] = dym.copy.banner[m];}
+    for( m in dym.email ) {dym.email[m] = dym.copy.email[m];}
+    console.log("Loaded old session data from local storage");
+  };
+  
+  dym.$onInit = function() {
+    if(localStorage.hasOwnProperty("dym")) dym.load();
+  };
+  
+  dym.flylg = true;
+  dym.flysm = true;
+  dym.flyout = "";
   
   
 });
+
+// TO do
+// Fix flyout filters and buttons and <banner-type>
---
+++ webform/emailabt.js
@@ -0,0 +1,26 @@
+(function(angular) {
+  
+  
+  function EmailController ($scope, $element, $attrs) {
+    var ctrl = this;
+
+    ctrl.$onInit = function() {
+      console.log("We're seriously starting this shiw");
+      ctrl.type = ctrl.type || "normal";
+      
+    }
+  }
+  
+  angular.module('dymApp')
+    
+    .component('emailabt', {
+
+    templateUrl : 'emailabt.html',
+    bindings: {
+      type:'@', // Banner type. Ex: HT, UT, PB, BB
+      email:'<'
+    },
+    controller : EmailController
+
+  });
+})(window.angular);
